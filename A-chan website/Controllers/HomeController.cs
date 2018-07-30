b489c0c3fb0cb9547a2ba56e4e27973013220ca6
AgeraZonda <nguyenquocduy1001@gmail.com>
2018-07-30 13:53:38 +0700

    30/7
    30/7

---
+++ A-chan website/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using A_chan_website.Models;
+
+namespace A_chan_website.Controllers
+{
+    public class HomeController : Controller
+    {
+        AChanDB db = new AChanDB();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Tutorial()
+        {
+            ViewBag.Message = "Tutorial";
+            return View();
+        }
+        public ActionResult Travel()
+        {
+            ViewBag.Message = "Travel";
+            return View();
+        }
+        public ActionResult Signin()
+        {
+            ViewBag.Message = "Signin";
+            return View();
+        }
+        public ActionResult Items()
+        {
+            ViewBag.Message = "Items";
+            var itemlist = db.SanPhams.Take(2).ToList();
+            return View(itemlist);
+        }
+
+    }
+}
---
+++ A-chan website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(A_chan_website.Startup))]
+namespace A_chan_website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
